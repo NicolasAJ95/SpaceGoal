6f28b0424657b3513ab742b6f7b5a99bc7e46081
Juan Pablo Amado <chumi1612@hotmail.com>
2019-02-20 17:30:54 -0500

    Animación Menú Principal
    Se añadió la animación y programación entre Canvas (MainMenu, Créditos, Niveles)

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] CanvasGroup canvasMenu, canvasNiveles, canvasCreditos;
+    Animator animMenu, animNiveles, animCreditos;
+
+    void Start()
+    {
+        animMenu = canvasMenu.GetComponent<Animator>();
+        animNiveles = canvasNiveles.GetComponent<Animator>();
+        animCreditos = canvasCreditos.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void CreditosFade (bool fade)
+    {
+        if (fade)
+            animCreditos.SetBool("Fade", true);
+        else
+            animCreditos.SetBool("Fade", false);
+    }
+    public void NivelesFade (bool fade)
+    {
+        if (fade)
+            animNiveles.SetBool("Fade", true);
+        else
+            animNiveles.SetBool("Fade", false);
+    }
+}
