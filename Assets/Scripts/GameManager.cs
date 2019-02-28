29d30d0fa565599db51c4d5a7f9f8299264d3348
NicolasAJ95 <nicolas.ajaramillo@live.com>
2019-02-28 01:23:51 -0500

    Inicio GameManager, Player
    Se empieza a programar el GameManager con las siguientes funciones:
    
    -Load Level
    -Start Level
    -Turn Count
    
    Se empieza a programar el jugador con un sistema de eventos para avisarle al gamemanager cuando inicia un nivel o realiza un turno.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private int TurnCount;
+
+
+    private void OnEnable()
+    {
+        Player.MoveAction += AddTurn;
+        Player.MoveAction += StartLevel;
+    }
+
+    private void OnDisable()
+    {
+        Player.MoveAction -= AddTurn;
+        Player.MoveAction -= StartLevel;
+    }
+
+    private void StartLevel()
+    {
+        TurnCount = 0;
+    }
+
+    private void AddTurn()
+    {
+        TurnCount += 1;
+    }
+
+    public void LoadLevel(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //LevelEvents
+    public delegate void LevelEvent();
+    public static event LevelEvent MoveAction;
+    public static event LevelEvent StartLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartLevel();
+    }
+
+    private void Move()
+    {
+       MoveAction();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
