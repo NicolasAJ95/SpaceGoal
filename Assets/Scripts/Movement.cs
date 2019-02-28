5a1fa2cdf563f794e7e69e7b8bf7fd5398c5afdd
Juan Pablo Amado <chumi1612@hotmail.com>
2019-02-28 16:53:53 -0500

    Implementación Partícula Nave

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        transform.position = transform.position + new Vector3(0.1f, 0, 0);
+    }
+}
