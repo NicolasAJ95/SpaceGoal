ae58c10d214ab69ab0b1dc1a9e05e1773d71f0b4
TrapReaper <jorgel.iglesias@hotmail.com>
2019-04-04 06:47:19 -0500

    Arreglo piso, verificaciones movimiento

--- Assets/Scripts/MovimientoNave.cs
+++ Assets/Scripts/MovimientoNave.cs
@@ -3,23 +3,47 @@
 public class MovimientoNave : MonoBehaviour
 {
     [SerializeField] GameObject portal;
+    [SerializeField] float force;
 
     private Vector3 posPortal;
+
+    bool pass;
+
+    
 
     void Start()
     {
         posPortal = portal.transform.position;
+        pass = false;
     }
 
 
     void Update()
     {
-        float posicionNave = transform.position.x;
-        float meta = posPortal.x;
-        if(posicionNave < meta)
+        if(pass == false)
         {
-            transform.position = transform.position + new Vector3(0.1f, 0, 0);
+            float posicionNave = transform.position.x;
+            float meta = posPortal.x;
+            if (posicionNave <= meta)
+            {
+                transform.position = transform.position + new Vector3(0.1f, 0, 0);
+            }
+            else
+            {
+                pass = true;
+            }
         }
+        else
+        {
+            float posicionNave = transform.position.x;
+            float meta = force;
+            if (posicionNave < meta)
+            {
+                transform.position = transform.position + new Vector3(0.1f, 0, 0);
+            }
+                
+        }
+        
         
     }
 }
