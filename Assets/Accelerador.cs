4c61d15242418bda5e17fdebff72fe215d25eab6
TrapReaper <jorgel.iglesias@hotmail.com>
2019-05-21 00:09:35 -0500

    Accelerador

---
+++ Assets/Accelerador.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Accelerador : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ship")
+        {
+            Vector3 speed = collision.gameObject.GetComponent<Rigidbody>().velocity;
+            collision.gameObject.GetComponent<Rigidbody>().velocity = speed * 1.5f;
+        }
+    }
+}
